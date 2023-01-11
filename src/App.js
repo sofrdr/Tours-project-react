2c16a2abe6df697b90add388a0cc3ae8328384d4
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-01-11 15:12:09 +0100

    Styling title and fetching data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import Tours from "./components/Tours";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body{
+  font-family: 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  line-height: 1.5;
+  color: #102a42; 
+  font-size: 1rem;
+}
+
+main{
+  width: 90vw;
+  max-width: 1170px;
+  margin: 5rem auto;
+}
+`;
 
 function App() {
   return (
     <main>
+      <GlobalStyle />
       <Tours />
     </main>
   );
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tour from "./Tour";
+import styled from "styled-components";
+
+const TitleContainer = styled.div`
+  text-align: center;
+  margin-bottom: 4rem;
+`;
+
+const StyledTitle = styled.h2`
+  letter-spacing: 0.1rem;
+  font-size: 2.5rem;
+  text-transform: capitalize;
+  margin-bottom: 0.75rem;
+`;
+
+const Underline = styled.div`
+  width: 6rem;
+  height: 0.25rem;
+  background: #49a6e9;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const Tours = () => {
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://course-api.com/react-tours-project"
+      );
+      const data = await response.json();
+      data && setTours(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section>
-      <h1>Our tours</h1>
+      <TitleContainer>
+        <StyledTitle>our tours</StyledTitle>
+        <Underline />
+      </TitleContainer>
+      {tours.map((tour) => {
+        const { id, name, info, image, price } = tour;
+        return (
+          <Tour key={id} name={name} info={info} image={image} price={price} />
+        );
+      })}
     </section>
   );
 };
