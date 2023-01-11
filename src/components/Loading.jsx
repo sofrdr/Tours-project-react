2c6ca747ed2545d51bf32891632da5e8ded01298
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-01-11 16:37:06 +0100

    Styling Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledLoading = styled.h1`
+  font-size: 3rem;
+  text-align: center;
+  text-transform: capitalize;
+  line-height: 1.25;
+  margin-bottom: 0.75rem;
+  letter-spacing: 0.1rem;
+`;
 
 const Loading = () => {
-  return <div>Loading ...</div>;
+  return (
+    <div>
+      <StyledLoading>Loading ...</StyledLoading>
+    </div>
+  );
 };
 
 export default Loading;
